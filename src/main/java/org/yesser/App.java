0853b82765460e5f03cdf5c4fce9c52ed8a1779b
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2021-10-16 10:41:41 -0600

    Estructura inicial del proyecto Patrón Builder

---
+++ src/main/java/org/yesser/App.java
@@ -0,0 +1,13 @@
+package org.yesser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
